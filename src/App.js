643a38d20a282dd38d36ffb882c5a1199c690a36
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-21 07:22:42 +0530

    added db url and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
-
+const url='https://my-project-1-22dd3-default-rtdb.firebaseio.com/users.json'
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button>post request</button>
     </div>
   );
 }

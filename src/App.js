34b22ac9ad2f0bcf674d7014816efd98c06e3912
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-21 07:55:54 +0530

    added crud html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "./style.css";
+// crud
+// create[post],read[get],update[patch/put],delete[delete]
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     insert form
+     update form
+     all data 
+     table
     </div>
   );
 }

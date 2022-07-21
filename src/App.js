ccf05e680e1b3ac56a9e3005ea5a906d31f29f14
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-21 07:32:21 +0530

    added db url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+const url = 'https://my-project-1-22dd3-default-rtdb.firebaseio.com/users.json';
 export default function App() {
   return (
     <div>

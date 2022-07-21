9cc2cfc4538d32258c2eec77c0947f9f776b7d8e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-21 08:00:27 +0530

    added instr form and events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+
 // crud
 // create[post],read[get],update[patch/put],delete[delete]
 
 export default function App() {
+  const [ob1, setob1] = useState({ name: '', email: '', gender: '' });
+  const handleChange1 = (e) => {
+    console.log(e.target.placeholder, e.target.value);
+  };
+  const _insert = () => {
+    // post request
+    const url = `https://my-project-1-22dd3-default-rtdb.firebaseio.com/users.json`;
+  };
   return (
     <div>
-     insert form
-     update form
-     all data 
-     table
+      <div>
+        <h3>insert form</h3>
+        <input placeholder="name" value={ob1.name} onChange={handleChange1} />
+        <input placeholder="email" value={ob1.email} onChange={handleChange1} />
+        <input
+          placeholder="gender"
+          value={ob1.gender}
+          onChange={handleChange1}
+        />
+        <button onClick={_insert}>insert</button>
+      </div>
+      update form all data table
     </div>
   );
 }

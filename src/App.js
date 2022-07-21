970771e96253da6c7ce9f486467cf6e5abda632d
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-21 07:26:05 +0530

    format for sedning post request added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import "./style.css";
-const url='https://my-project-1-22dd3-default-rtdb.firebaseio.com/users.json'
+import React from 'react';
+import axios from 'axios';
+const url = 'https://my-project-1-22dd3-default-rtdb.firebaseio.com/users.json';
 export default function App() {
+  const handleClick = () => {
+    axios
+      .post(url, data)
+      .then((res) => res.data)
+      .then((d) => console.log('after adding', d))
+      .catch((e) => console.log('after error', e))
+      .finally(() => console.log('finally'));
+  };
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <button>post request</button>
+      <button onClick={handleClick}>post request</button>
     </div>
   );
 }

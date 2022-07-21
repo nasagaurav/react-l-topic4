1b19f4b5702cc9dbb9f7be154344174eba684643
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-21 08:05:44 +0530

    crud html completed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import axios from 'axios';
 // create[post],read[get],update[patch/put],delete[delete]
 
 export default function App() {
+  const [a, seta] = useState([]);
   const [ob1, setob1] = useState({ name: '', email: '', gender: '' });
+  const [ob2, setob2] = useState({ id: '', name: '', email: '', gender: '' });
   const handleChange1 = (e) => {
-    console.log(e.target.placeholder, e.target.value);
+    console.log('ob1', e.target.placeholder, e.target.value); //insert/create/ob1
   };
+  const handleChange2 = (e) => {
+    console.log('ob2', e.target.placeholder, e.target.value); //edit/update/ob2
+  };
+
   const _insert = () => {
     // post request
     const url = `https://my-project-1-22dd3-default-rtdb.firebaseio.com/users.json`;
+  };
+
+  const _update = () => {
+    const url = `https://my-project-1-22dd3-default-rtdb.firebaseio.com/users/{-N7TT_k_BbSLdHfwszx6 ?}.json`;
   };
   return (
     <div>
@@ -26,7 +36,41 @@ export default function App() {
         />
         <button onClick={_insert}>insert</button>
       </div>
-      update form all data table
+      <div>
+        <h3>update form</h3>
+        <input placeholder="name" value={ob2.name} onChange={handleChange2} />
+        <input placeholder="email" value={ob2.email} onChange={handleChange2} />
+        <input
+          placeholder="gender"
+          value={ob2.gender}
+          onChange={handleChange2}
+        />
+        <button onClick={_update}>update</button>
+      </div>
+      <h1>all users {a.length} </h1>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>gender</th>
+            <th>actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>item.id</td>
+            <td>item.name</td>
+            <td>item.email</td>
+            <td>item.gender</td>
+            <td>
+              <button>edit</button>
+              <button>delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
